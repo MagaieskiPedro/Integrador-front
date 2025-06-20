1e50e66b4116195230e99cdd7485147e4f251b85
PedroMagaieski <magaieskipedro@gmail.com>
2025-06-20 07:48:24 -0300

    acabando graças a Deus

--- src/Paginas/Login.jsx
+++ src/Paginas/Login.jsx
@@ -1,17 +1,72 @@
 import estilo from "./Login.module.css"
+import axios from 'axios';
+import React, {use, useState} from 'react';
+
+
+import {useForm} from 'react-hook-form';
+import {z} from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {data, useNavigate} from 'react-router-dom'
+
+// URL base da api
+const API_URL = 'http://127.0.0.1:8000'
+
+// Validação zod
+const schemaLogin = z.object({
+    username: z.string()
+        .min(1,'Informe seu usuário')
+        .max(15, 'Informe um usuário de até 15 caracteres'),
+    password: z.string()
+        .min(1,'Informe sua senha')
+        .max(15, 'Informe uma senha de até 15 caracteres'),
+})
 
 export function Login(){
+    // Navegação
+    const navigate = useNavigate();
+    
+    // Recebe dados do form e valida ou retorna erro
+    const{
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({
+        resolver: zodResolver(schemaLogin)
+    })
+
+    //  Função de post de dados validados
+    const enviarDadosFormulario = async(data) =>{
+        console.log(`Dados: ${data}`)
+        try{
+            const response = await axios.post(`${API_URL}/api/login/`,{
+                            username: data.username,
+                            password: data.password
+                        });
+            const {access, refresh, usuario } = response.data;
+            localStorage.setItem('access_token', access);
+            localStorage.setItem('refresh_token', refresh);
+            localStorage.setItem('categoria',usuario.categoria);
+            navigate('/')
+        }catch(error){
+            console.error('erro no login', error);
+            alert('credenciais inválidas')
+        }
+    }
 
     return(
         <main className={estilo.container}>
             <section>
                 <h1>Faça login: </h1>
-                <form action="">
+                <form onSubmit={handleSubmit(enviarDadosFormulario)}>
                     <label htmlFor="username">Nome: </label>
-                    <input type="text" name="username" placeholder="Digite seu nome" />
+                    <input {...register('username')} type="text" name="username" placeholder="Digite seu nome" />
+                    {errors.username && <p className={estilo.erro}>{errors.username.message}</p>}
+                    
                     <label htmlFor="password">Senha: </label>
-                    <input type="password" name="password" placeholder="Digite sua senha" />
-                    <button>Logar</button>
+                    <input {...register('password')} type="password" name="password" placeholder="Digite sua senha" />
+                    {errors.password && <p className={estilo.erro}>{errors.password.message}</p>}
+
+                    <button type="submit">Logar</button>
                 </form>
                 
             </section>
--- src/Paginas/Sensores.jsx
+++ src/Paginas/Sensores.jsx
@@ -1,9 +1,76 @@
 import estilo from "./Sensores.module.css"
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileCirclePlus,faPenToSquare,faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+
+import {z} from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const API_URL = 'http://127.0.0.1:8000';
+
+const schemaSensor = z.object({
+    sensor: z.string()
+        .min(1,'Informe o nome do sensor')
+        .max(50, 'Informe um nome de sensor de até 50 caracteres'),
+    mac_address: z.string()
+        .min(1,'Informe o endereço mac')
+        .max(50, 'Informe um endereço mac de até 50 caracteres'),
+    unidade_med: z.string()
+        .min(1,'Informe a unidade de medida')
+        .max(50, 'Informe uma unidade de medida de até 50 caracteres'),
+    latitude: z.string()
+        .min(1,'Informe a latitude do sensor')
+        .max(50, 'Informe uma latitude de até 50 caracteres'),
+    longitude: z.string()
+        .min(1,'Informe a longitude do sensor')
+        .max(50, 'Informe uma longitude de até 50 caracteres'),
+    status: z.string()
+        .min(1,'Informe o Status')
+        .max(50, 'Informe um Status de até 50 caracteres'),
+})
 
 export function Sensores(){
+    // Hook de dados
+    const [data, setData] = useState([]);
+
+    // token de acesso
+    const access_token = localStorage.getItem('access_token')
+
+    // Metodos de Get e Delete
+    const obterDados = async() => {
+        try {
+            const response = await axios.get(`${API_URL}/api/sensores/`, {
+                headers: {
+                    'Authorization': `Bearer ${access_token}`,
+                    'Content-Type': 'application/json'
+                  }
+            });
+            setData(response.data)
+        }catch (error) {
+            console.log('error: ', error)
+        }
+    };
+
+    const deletarDado = async (IDNTY) => {
+        try{
+           const response = await axios.delete(`${API_URL}/api/sensores/${IDNTY}`, {
+                headers: {
+                    'Authorization': `Bearer ${access_token}`,
+                    'Content-Type': 'application/json'
+                  }
+            });
+            obterDados();
+        }catch (error) {
+            console.log('errinho: ', IDNTY, " - ",  error)
+        }
+    }
+    
+    useEffect(() => {
+        obterDados();
+    }, []);
 
     return(
         <main className={estilo.container}>
@@ -15,6 +82,7 @@ export function Sensores(){
                          Novo registro
                     </button>
                 </Link>
+                {data.length > 0 ? ( 
                 <table>
                     <thead>
                         <tr>
@@ -29,97 +97,35 @@ export function Sensores(){
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Sensor</td>
-                            <td>Endereço mac</td>
-                            <td>Unidade de medida</td>
-                            <td>Latitude</td>
-                            <td>Longitude</td>
-                            <td>Status</td>
+                        {data.map ((sensor) =>(
+                            <tr key={sensor.id}>
+                                <td>{sensor.sensor}</td>
+                                <td>{sensor.mac_address}</td>
+                                <td>{sensor.unidade_med}</td>
+                                <td>{sensor.latitude}</td>
+                                <td>{sensor.longitude}</td>
+                                <td>{sensor.status}</td>
 
-                            {/* ${item.id} */}
-                            <Link to={`/sensores/1`} >
-                                <td><button className={estilo.editar}><FontAwesomeIcon icon={faPenToSquare} /></button></td> 
-                            </Link>
-                            <td><button className={estilo.deletar}><FontAwesomeIcon icon={faTrash}  size="sm"/></button></td> 
-                        </tr>
-                        <tr>
-                            <td>Sensor</td>
-                            <td>Endereço mac</td>
-                            <td>Unidade de medida</td>
-                            <td>Latitude</td>
-                            <td>Longitude</td>
-                            <td>Status</td>
-
-                            {/* ${item.id} */}
-                            <Link to={`/sensores/1`} >
-                                <td><button className={estilo.editar}><FontAwesomeIcon icon={faPenToSquare} /></button></td> 
-                            </Link>
-                            <td><button className={estilo.deletar}><FontAwesomeIcon icon={faTrash}  size="sm"/></button></td> 
-                        </tr>
-                        <tr>
-                            <td>Sensor</td>
-                            <td>Endereço mac</td>
-                            <td>Unidade de medida</td>
-                            <td>Latitude</td>
-                            <td>Longitude</td>
-                            <td>Status</td>
-
-                            {/* ${item.id} */}
-                            <Link to={`/sensores/1`} >
-                                <td><button className={estilo.editar}><FontAwesomeIcon icon={faPenToSquare} /></button></td> 
-                            </Link>
-                            <td><button className={estilo.deletar}><FontAwesomeIcon icon={faTrash}  size="sm"/></button></td> 
-                        </tr>
-                        <tr>
-                            <td>Sensor</td>
-                            <td>Endereço mac</td>
-                            <td>Unidade de medida</td>
-                            <td>Latitude</td>
-                            <td>Longitude</td>
-                            <td>Status</td>
-
-                            {/* ${item.id} */}
-                            <Link to={`/sensores/1`} >
-                                <td><button className={estilo.editar}><FontAwesomeIcon icon={faPenToSquare} /></button></td> 
-                            </Link>
-                            <td><button className={estilo.deletar}><FontAwesomeIcon icon={faTrash}  size="sm"/></button></td> 
-                        </tr>
-                        <tr>
-                            <td>Sensor</td>
-                            <td>Endereço mac</td>
-                            <td>Unidade de medida</td>
-                            <td>Latitude</td>
-                            <td>Longitude</td>
-                            <td>Status</td>
-
-                            {/* ${item.id} */}
-                            <Link to={`/sensores/1`} >
-                                <td><button className={estilo.editar}><FontAwesomeIcon icon={faPenToSquare} /></button></td> 
-                            </Link>
-                            <td><button className={estilo.deletar}><FontAwesomeIcon icon={faTrash}  size="sm"/></button></td> 
-                        </tr>
-                        <tr>
-                            <td>Sensor</td>
-                            <td>Endereço mac</td>
-                            <td>Unidade de medida</td>
-                            <td>Latitude</td>
-                            <td>Longitude</td>
-                            <td>Status</td>
-
-                            {/* ${item.id} */}
-                            <Link to={`/sensores/1`} >
-                                <td><button className={estilo.editar}><FontAwesomeIcon icon={faPenToSquare} /></button></td> 
-                            </Link>
-                            <td><button className={estilo.deletar}><FontAwesomeIcon icon={faTrash}  size="sm"/></button></td> 
-                        </tr>
-                        
-                        
+                                {/* ${item.id} */}
+                                <Link to={`/sensores/${sensor.id}`} >
+                                    <td><button className={estilo.editar}><FontAwesomeIcon icon={faPenToSquare} /></button></td> 
+                                </Link>
+                                <td><button className={estilo.deletar}><FontAwesomeIcon icon={faTrash}  size="sm"/></button></td> 
+                            </tr>
+                        ))}
+   
                         
                     </tbody>
 
                 </table>
-                
+                ) : (
+            <table>
+                <tr>
+                    <th>Sem Dados aqui</th>
+                </tr>
+            </table>
+            )}
+
             </section>
 
         </main>
